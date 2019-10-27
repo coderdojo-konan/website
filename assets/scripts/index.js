f7605d75f1d2c32cd9d0e8428a2f2ad3aa0e5538
YuSan <ysoga19@gmail.com>
2019-10-27 19:04:55 +0900

    お問い合わせフォームを追加

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -10,3 +10,64 @@ $(document).ready(function(){
     }
   })
 })
+
+$(document).ready(function() {
+  $('#contact-form').submit(function(e){
+    let flag = true;
+
+    e.preventDefault();
+
+    $('.error').text('')
+    $('.message').text('')
+    $('.message').removeClass('success-message')
+    $('.message').removeClass('error-message')
+
+    const data = {
+      name: $('#contact-form [name=name]').val(),
+      email: $('#contact-form [name=email]').val(),
+      subject: $('#contact-form [name=subject]').val(),
+      body: $('#contact-form [name=body]').val(),
+      recaptchaToken: $('#contact-form [name=recaptcha-token]').val()
+    }
+
+    if (!data.name) {
+      $('.error-name').text('この項目は必須です。').addClass('is-danger')
+      $('#contact-form [name=name]').addClass('is-danger')
+      flag = false
+    }
+    if (!data.email) {
+      $('.error-email').text('この項目は必須です。').addClass('is-danger')
+      $('#contact-form [name=email]').addClass('is-danger')
+      flag = false
+    }
+    if (!data.subject) {
+      $('.error-subject').text('この項目は必須です。').addClass('is-danger')
+      $('#contact-form [name=subject]').addClass('is-danger')
+      flag = false
+    }
+    if (!data.body) {
+      $('.error-body').text('この項目は必須です。')
+      $('#contact-form [name=body]').addClass('is-danger')
+      flag = false
+    }
+
+    if (flag) {
+      $.post('http://localhost:3000/contact', data)
+        .done(function(res) {
+          if (res === 'success') {
+            $('.message').text('送信に成功しました！')
+            $('.message').addClass('success-message')
+          } else {
+            $('.message').text('送信に失敗しました。もう一度お試しください。')
+            $('.message').addClass('error-message')
+          }
+        })
+
+      grecaptcha.execute('6LeDrL8UAAAAAN0WsBFZK60QCR7E4QEjSOFUCM_-', {action: 'homepage'}).then(function(token) {
+        $('.recaptcha-token').val(token);
+      })
+    } else {
+      return flag
+    }
+  })
+});
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,3 +5,9 @@ $(document).ready(() => {
 $('.navbar-burger').on('click', e => {
   $('#navBurger').hasClass('is-active') ? $('#navBurger').removeClass('is-active') : $('#navBurger').addClass('is-active')
 })
+
+grecaptcha.ready(function() {
+  grecaptcha.execute('6LeDrL8UAAAAAN0WsBFZK60QCR7E4QEjSOFUCM_-', {action: 'homepage'}).then(function(token) {
+    $('.recaptcha-token').val(token);
+  });
+});
